c71436761a7e2d71ea104c67f41ead0f0930ccbe
EstelleJiao <estellejiaoo@gmail.com>
2023-04-05 21:53:07 +0800

    Add ceres_test

--- ICP/src/ceres_test.cc
+++ ICP/src/ceres_test.cc
@@ -11,84 +11,50 @@ using ceres::CostFunction;
 using ceres::Problem;
 using ceres::Solve;
 using ceres::Solver;
-
-struct PointCloudResidual {
-  PointCloudResidual(double x, double y, double z) : x_(x), y_(y), z_(z) {}
-  template <typename T>
-  bool operator()(const T *const x, const T *const y, const T *const z,
-                  T *residual) const {
-    residual[0] = x_ - x[0];
-    residual[1] = y_ - y[0];
-    residual[2] = z_ - z[0];
-    return true;
-  }
-
- private:
-  const double x_;
-  const double y_;
-  const double z_;
-};
-
-// int main(int argc, char** argv) {
-//   google::InitGoogleLogging(argv[0]);
-//   Problem problem;
-//   Eigen::Vector3d Point_1(1, 1, 1);
-//   Eigen::Vector3d Point_2(-2, -2, -2);
-//   std::vector<Eigen::Vector3d> points;
-//   points.push_back(Point_1);
-//   points.push_back(Point_2);
-
-//   Eigen::Vector3d initial_guess(0, 0, 0);
-
-//   for (const auto& point : points) {
-//     problem.AddResidualBlock(
-//         new AutoDiffCostFunction<xyzResidual, 3, 1, 1, 1>(
-//             new xyzResidual(point.x(), point.y(), point.z())),
-//         nullptr, &initial_guess.x(), &initial_guess.y(), &initial_guess.z());
-//   }
-//   Solver::Options options;
-//   options.max_num_iterations = 25;
-//   options.linear_solver_type = ceres::DENSE_QR;
-//   options.minimizer_progress_to_stdout = true;
-//   Solver::Summary summary;
-//   Solve(options, &problem, &summary);
-//   std::cout << summary.BriefReport() << "\n";
-//   //   std::cout << "Initial m: " << 0.0 << " c: " << 0.0 << "\n";
-//   std::cout << "Final   x: " << initial_guess.x() << " y: " <<
-//   initial_guess.y()
-//             << " z: " << initial_guess.z() << "\n";
-//   return 0;
-// }
+using PointPair = std::pair<Eigen::Vector3d, Eigen::Vector3d>;
 
 class SE3SophusParameterization : public ceres::LocalParameterization {
  public:
+  SE3SophusParameterization() {}
   virtual ~SE3SophusParameterization() {}
 
-  virtual bool Plus(const double *x, const double *delta,
-                    double *x_plus_delta) const override {
-    // 转化为Eigen变量
-    const Eigen::Map<const Sophus::SE3d> T(x);
-    Eigen::Map<Sophus::SE3d> T_plus_delta(x_plus_delta);
-    const Eigen::Map<const Eigen::Vector3d> delta_phi(delta);  // 先旋转部分
-    const Eigen::Map<const Eigen::Vector3d> delta_rho(delta + 3);  // 再平移部分
+  virtual bool Plus(const double *T_raw, const double *delta_raw,
+                    double *T_plus_delta_raw) const override {
+    // Eigen::Map is like a reference to the original data
+    // T_raw is a Sophux::SE group element
+    const Eigen::Map<const Sophus::SE3d> T(T_raw);
+    Eigen::Map<Sophus::SE3d> T_plus_delta(T_plus_delta_raw);
+
+    // delta_raw is a 6x1 vector
+    // we need to change the sequence for updating
+    const Eigen::Map<const Eigen::Vector3d> delta_phi(delta_raw);  // 先旋转部分
+    const Eigen::Map<const Eigen::Vector3d> delta_rho(delta_raw +
+                                                      3);  // 再平移部分
 
     Eigen::Matrix<double, 6, 1> delta_se3;
     delta_se3.block<3, 1>(0, 0) = delta_rho;
     delta_se3.block<3, 1>(3, 0) =
         delta_phi;  // 但进行exp()操作时，平移在前，旋转在后
 
-    // 左乘更新
+    // Left multiply
     T_plus_delta = Sophus::SE3d::exp(delta_se3) * T;
 
     return true;
   }
 
-  virtual bool ComputeJacobian(const double *x,
-                               double *jacobian) const override {
-    Eigen::Map<Eigen::Matrix<double, 7, 6, Eigen::RowMajor> > jaco_tmp(
-        jacobian);
-    jaco_tmp.setZero();
-    jaco_tmp.topRows(6).setIdentity();
+  // The jacobian of Plus(x, delta) w.r.t delta at delta = 0. -> ?
+  virtual bool ComputeJacobian(const double *T_raw,
+                               double *jacobian_raw) const override {
+    Eigen::Map<Sophus::SE3d const> T(T_raw);
+    // How to define a 3 by 6 Jacobian here?
+    Eigen::Map<Eigen::Matrix<double, 3, 6, Eigen::RowMajor>> jacobian(
+        jacobian_raw);
+    jacobian.setZero();
+
+    // Set the left 3 by 3 to an Identity
+    // Set the right 3 by 3 to an Inverse Identity
+    jacobian.block<3, 3>(0, 0) = Eigen::Matrix3d::Identity();
+    jacobian.block<3, 3>(0, 3) = Eigen::Matrix3d::Identity();
     return true;
   }
 
@@ -97,6 +63,36 @@ class SE3SophusParameterization : public ceres::LocalParameterization {
   }
 
   virtual int LocalSize() const override { return Sophus::SE3d::DoF; }
+};
+
+class PointCostFunction : public ceres::SizedCostFunction<3, 7> {
+  // size of the residual, the transform
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+  PointCostFunction(const PointPair &point_pair) : point_pair_(point_pair) {}
+  virtual bool Evaluate(double const *const *parameters, double *residuals,
+                        double **jacobians) const {
+    // The parameters is an SE(3) transform -> len(12) array
+    Eigen::Map<const Sophus::SE3d> T(parameters[0]);
+
+    // compute the residual
+    Eigen::Vector3d z = T * point_pair_.second;
+    Eigen::Vector3d point_diff = point_pair_.first - z;
+    residuals[0] = point_diff.x();
+    residuals[0] = point_diff.y();
+    residuals[0] = point_diff.z();
+
+    // populate the jacobian matrix
+    Eigen::Map<Eigen::Matrix<double, 3, 6, Eigen::RowMajor>> jacobian(
+        jacobians[0]);
+    jacobian.block<3, 3>(0, 0) = Sophus::SO3d::hat(z);
+    jacobian.block<3, 3>(0, 3) = Eigen::DiagonalMatrix<double, 3>(-1, -1, -1);
+    return true;
+  }
+
+ private:
+  // pairs of (target, source)
+  PointPair point_pair_;
 };
 
 int main() { return 0; }
